437e9a55ff3527c39325dd4aa73e4524579ef2f4
Raí Lima <rai160493@hotmail.com>
2019-11-16 10:18:00 -0300

    Add minhas simulações ao routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SimularCompraComponent } from './pages/simular-compra/simular-compra.component';
+import { MinhasSimulacoesComponent } from './pages/minhas-simulacoes/minhas-simulacoes.component';
+import { MinhasSimulacoesResolver } from './pages/minhas-simulacoes/minhas-simulacoes-resolver.service';
 
 
 const routes: Routes = [
   { path: 'simulacao-compra', component: SimularCompraComponent },
+  { path: 'minhas-simulacoes', component: MinhasSimulacoesComponent, resolve: {
+    simulacoes: MinhasSimulacoesResolver
+  } },
   {
     path: '**',
     redirectTo: '/simulacao-compra',
